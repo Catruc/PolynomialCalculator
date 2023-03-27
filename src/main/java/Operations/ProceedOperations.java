4e6dd8e8e6c35a0322056c53038f0b5e199c85e4
Catruc <dan.catruc@gmail.com>
2023-03-27 20:23:16 +0300

    project ready

--- src/main/java/Operations/ProceedOperations.java
+++ src/main/java/Operations/ProceedOperations.java
@@ -11,146 +11,146 @@ import static java.lang.System.exit;
 
 public class ProceedOperations {
     public Polynomial add(Polynomial p1, Polynomial p2) {
-        Polynomial result = new Polynomial();
-        for (Map.Entry<Integer, Monomial> entry : p1.getPolynomial().entrySet()) {
-            int power = entry.getKey();
-            double coefficient = entry.getValue().getCoefficient();
-            if (p2.getPolynomial().containsKey(power)) {
-                coefficient += p2.getPolynomial().get(power).getCoefficient();
+        Polynomial result = new Polynomial();   /// the result of the addition
+        for (Map.Entry<Integer, Monomial> entry : p1.getPolynomial().entrySet()) {   /// iterate through the first polynomial
+            int power = entry.getKey();   /// get the power of the current monomial
+            double coefficient = entry.getValue().getCoefficient();    /// get the coefficient of the current monomial
+            if (p2.getPolynomial().containsKey(power)) {    /// if the second polynomial contains the current power
+                coefficient += p2.getPolynomial().get(power).getCoefficient();   /// add the coefficients
             }
-            if (coefficient != 0) {
-                Monomial monomial = new Monomial(power, coefficient);
-                result.addMonomial(monomial);
-            }
-        }
-        for (Map.Entry<Integer, Monomial> entry : p2.getPolynomial().entrySet()) {
-            int power = entry.getKey();
-            double coefficient = entry.getValue().getCoefficient();
-            if (!p1.getPolynomial().containsKey(power)) {
-                Monomial monomial = new Monomial(power, coefficient);
-                result.addMonomial(monomial);
+            if (coefficient != 0) {    /// if the coefficient is not 0
+                Monomial monomial = new Monomial(power, coefficient);   /// create a new monomial
+                result.addMonomial(monomial);    /// add the monomial to the result
             }
         }
-        return result;
+        for (Map.Entry<Integer, Monomial> entry : p2.getPolynomial().entrySet()) {   /// iterate through the second polynomial
+            int power = entry.getKey();   /// get the power of the current monomial
+            double coefficient = entry.getValue().getCoefficient();     /// get the coefficient of the current monomial
+            if (!p1.getPolynomial().containsKey(power)) {   /// if the first polynomial does not contain the current power
+                Monomial monomial = new Monomial(power, coefficient);    /// create a new monomial
+                result.addMonomial(monomial);   /// add the monomial to the result
+            }
+        }
+        return result;   /// return the result
     }
 
     public Polynomial substract(Polynomial p1, Polynomial p2) {
-        Polynomial result = new Polynomial();
-        for (Map.Entry<Integer, Monomial> entry : p1.getPolynomial().entrySet()) {
-            int power = entry.getKey();
-            double coefficient = entry.getValue().getCoefficient();
-            if (p2.getPolynomial().containsKey(power)) {
-                coefficient -= p2.getPolynomial().get(power).getCoefficient();
+        Polynomial result = new Polynomial();   /// the result of the substraction
+        for (Map.Entry<Integer, Monomial> entry : p1.getPolynomial().entrySet()) {    /// iterate through the first polynomial
+            int power = entry.getKey();   /// get the power of the current monomial
+            double coefficient = entry.getValue().getCoefficient();   /// get the coefficient of the current monomial
+            if (p2.getPolynomial().containsKey(power)) {   /// if the second polynomial contains the current power
+                coefficient -= p2.getPolynomial().get(power).getCoefficient();   /// substract the coefficients
             }
-            if (coefficient != 0) {
-                Monomial monomial = new Monomial(power, coefficient);
-                result.addMonomial(monomial);
+            if (coefficient != 0) {   /// if the coefficient is not 0
+                Monomial monomial = new Monomial(power, coefficient);    /// create a new monomial
+                result.addMonomial(monomial);   /// add the monomial to the result
             }
         }
-        for (Map.Entry<Integer, Monomial> entry : p2.getPolynomial().entrySet()) {
-            int power = entry.getKey();
-            double coefficient = entry.getValue().getCoefficient();
-            if (!p1.getPolynomial().containsKey(power)) {
+        for (Map.Entry<Integer, Monomial> entry : p2.getPolynomial().entrySet()) {   /// iterate through the second polynomial
+            int power = entry.getKey();   /// get the power of the current monomial
+            double coefficient = entry.getValue().getCoefficient();   /// get the coefficient of the current monomial
+            if (!p1.getPolynomial().containsKey(power)) {   /// if the first polynomial does not contain the current power
                 coefficient = -coefficient; // negate the coefficient
-                Monomial monomial = new Monomial(power, coefficient);
-                result.addMonomial(monomial);
+                Monomial monomial = new Monomial(power, coefficient);  /// create a new monomial
+                result.addMonomial(monomial);  /// add the monomial to the result
             }
         }
-        return result;
+        return result;   /// return the result
     }
 
     public Polynomial multiply(Polynomial p1, Polynomial p2) {
-        Polynomial resultMultiply = new Polynomial();
-        for (Map.Entry<Integer, Monomial> entry : p1.getPolynomial().entrySet()) {
-            int power1 = entry.getKey();
-            double coefficient1 = entry.getValue().getCoefficient();
-            for (Map.Entry<Integer, Monomial> entry2 : p2.getPolynomial().entrySet()) {
-                int power2 = entry2.getKey();
-                double coefficient2 = entry2.getValue().getCoefficient();
-                int power = power1 + power2;
-                double coefficient = coefficient1 * coefficient2;
-                if (resultMultiply.getPolynomial().containsKey(power)) {
-                    coefficient += resultMultiply.needCoefficient(power);
+        Polynomial resultMultiply = new Polynomial();   /// the result of the multiplication
+        for (Map.Entry<Integer, Monomial> entry : p1.getPolynomial().entrySet()) {   /// iterate through the first polynomial
+            int power1 = entry.getKey();   /// get the power of the current monomial
+            double coefficient1 = entry.getValue().getCoefficient();   /// get the coefficient of the current monomial
+            for (Map.Entry<Integer, Monomial> entry2 : p2.getPolynomial().entrySet()) {   /// iterate through the second polynomial
+                int power2 = entry2.getKey();   /// get the power of the current monomial
+                double coefficient2 = entry2.getValue().getCoefficient();    /// get the coefficient of the current monomial
+                int power = power1 + power2;   /// calculate the power of the new monomial
+                double coefficient = coefficient1 * coefficient2;   /// calculate the coefficient of the new monomial
+                if (resultMultiply.getPolynomial().containsKey(power)) {  /// if the result polynomial contains the current power
+                    coefficient += resultMultiply.needCoefficient(power);   /// add the coefficients
                 }
-                if (coefficient != 0) {
-                    Monomial monomial = new Monomial(power, coefficient);
-                    resultMultiply.addMonomial(monomial);
+                if (coefficient != 0) {  /// if the coefficient is not 0
+                    Monomial monomial = new Monomial(power, coefficient);  /// create a new monomial
+                    resultMultiply.addMonomial(monomial);   /// add the monomial to the result
                 }
             }
         }
-        return resultMultiply;
+        return resultMultiply;  /// return the result
     }
 
     public Polynomial derivative(Polynomial polynomial) {
-        Polynomial resultDerivative = new Polynomial();
-        for (Map.Entry<Integer, Monomial> entry : polynomial.getPolynomial().entrySet()) { // start from power 1
-            int maxPower = entry.getKey();
-            double coefficient = entry.getValue().getCoefficient();
-            if (coefficient != 0) {
-                Monomial monomial = new Monomial(maxPower - 1, coefficient * maxPower);
-                resultDerivative.addMonomial(monomial);
+        Polynomial resultDerivative = new Polynomial();   /// the result of the derivative
+        for (Map.Entry<Integer, Monomial> entry : polynomial.getPolynomial().entrySet()) {  /// start from power 1
+            int maxPower = entry.getKey();  /// get the power of the current monomial
+            double coefficient = entry.getValue().getCoefficient();  /// get the coefficient of the current monomial
+            if (coefficient != 0) {  /// if the coefficient is not 0
+                Monomial monomial = new Monomial(maxPower - 1, coefficient * maxPower);   /// create a new monomial
+                resultDerivative.addMonomial(monomial);   /// add the monomial to the result
             }
         }
         return resultDerivative;
     }
 
     public Polynomial integration(Polynomial polynomial) {
-        Polynomial resultIntegration = new Polynomial();
-        for (Map.Entry<Integer, Monomial> entry : polynomial.getPolynomial().entrySet()) { // start from power 1
-            int maxPower = entry.getKey();
-            double coefficient = entry.getValue().getCoefficient();
-            if (coefficient != 0) {
-                Monomial monomial = new Monomial(maxPower + 1, coefficient / (maxPower + 1));
-                resultIntegration.addMonomial(monomial);
+        Polynomial resultIntegration = new Polynomial();   /// the result of the integration
+        for (Map.Entry<Integer, Monomial> entry : polynomial.getPolynomial().entrySet()) { /// start from power 1
+            int maxPower = entry.getKey();      /// get the power of the current monomial
+            double coefficient = entry.getValue().getCoefficient();  /// get the coefficient of the current monomial
+            if (coefficient != 0) {  /// if the coefficient is not 0
+                Monomial monomial = new Monomial(maxPower + 1, coefficient / (maxPower + 1));  /// create a new monomial
+                resultIntegration.addMonomial(monomial);   /// add the monomial to the result
             }
         }
-        return resultIntegration;
+        return resultIntegration;   /// return the result
     }
 
     public ArrayList<Polynomial> dividePolynomials(Polynomial divident, Polynomial divisor) throws ArithmeticException, IllegalArgumentException {
-        if (divisor.getPolynomial().isEmpty() || divisor.getPolynomial().values().stream().allMatch(c -> c.getCoefficient() == 0)) {
-            JOptionPane.showMessageDialog(null, "Division by zero");
+        if (divisor.getPolynomial().isEmpty() || divisor.getPolynomial().values().stream().allMatch(c -> c.getCoefficient() == 0)) {  /// if the divisor is 0
+            JOptionPane.showMessageDialog(null, "Division by zero");  /// show a message
         }
-        Polynomial quotient = new Polynomial();
-        Polynomial remainder = new Polynomial();
-        int dividendDegree = divident.getDegree();
-        int divisorDegree = divisor.getDegree();
-        if (dividendDegree < divisorDegree) {
-            JOptionPane.showMessageDialog(null, "Dividend degree is smaller than divisor degree");
+        Polynomial quotient = new Polynomial();   /// the quotient
+        Polynomial remainder = new Polynomial();  /// the remainder
+        int dividendDegree = divident.getDegree();  /// get the degree of the dividend
+        int divisorDegree = divisor.getDegree();   /// get the degree of the divisor
+        if (dividendDegree < divisorDegree) {  /// if the dividend degree is smaller than the divisor degree
+            JOptionPane.showMessageDialog(null, "Dividend degree is smaller than divisor degree");  /// show a message
         }
-        if (divisor.getDegree() == 0) {
-            for (Map.Entry<Integer, Monomial> entry : divident.getPolynomial().entrySet()) {
-                int power = entry.getKey();
-                double coefficient = entry.getValue().getCoefficient() / divisor.needCoefficient(0);
-                quotient.addMonomial(new Monomial(power, coefficient));
+        if (divisor.getDegree() == 0) {   /// if the divisor degree is 0
+            for (Map.Entry<Integer, Monomial> entry : divident.getPolynomial().entrySet()) {  /// iterate through the dividend
+                int power = entry.getKey();   /// get the power of the current monomial
+                double coefficient = entry.getValue().getCoefficient() / divisor.needCoefficient(0);   /// get the coefficient of the current monomial
+                quotient.addMonomial(new Monomial(power, coefficient));  /// add the monomial to the quotient
             }
-            return new ArrayList<Polynomial>() {{
+            return new ArrayList<Polynomial>() {{  /// return the quotient and the remainder
                 add(quotient);
                 add(remainder);
             }};
         }
-        while (dividendDegree >= divisorDegree) {
-            if (divisor.needCoefficient(divisorDegree) == 0) {
-                JOptionPane.showMessageDialog(null, "Divisor coefficient is zero");
-                break;
+        while (dividendDegree >= divisorDegree) {  /// while the dividend degree is bigger than the divisor degree
+            if (divisor.needCoefficient(divisorDegree) == 0) {   /// if the divisor coefficient is 0
+                JOptionPane.showMessageDialog(null, "Divisor coefficient is zero");  /// show a message
+                break;  /// break the loop
             }
-            double quotientCoefficient = divident.needCoefficient(dividendDegree) / divisor.needCoefficient(divisorDegree);
-            Monomial quotientMonomial = new Monomial(dividendDegree - divisorDegree, quotientCoefficient);
-            quotient.addMonomial(quotientMonomial);
-            Polynomial product = new Polynomial();
-            product.addMonomial(quotientMonomial);
-            product = multiply(product, divisor);
-            Polynomial difference = substract(divident, product);
-            divident.getPolynomial().clear();
-            divident.getPolynomial().putAll(difference.getPolynomial());
-            dividendDegree = divident.getDegree();
+            double quotientCoefficient = divident.needCoefficient(dividendDegree) / divisor.needCoefficient(divisorDegree);  /// calculate the quotient coefficient
+            Monomial quotientMonomial = new Monomial(dividendDegree - divisorDegree, quotientCoefficient);  /// create a new monomial
+            quotient.addMonomial(quotientMonomial);  /// add the quotientMonomial to the quotient
+            Polynomial product = new Polynomial();   /// the product
+            product.addMonomial(quotientMonomial);  /// add the quotientMonomial to the product
+            product = multiply(product, divisor);   /// multiply the product with the divisor
+            Polynomial difference = substract(divident, product);  /// substract the product from the dividend
+            divident.getPolynomial().clear();  /// clear the dividend
+            divident.getPolynomial().putAll(difference.getPolynomial());  /// add the difference to the dividend
+            dividendDegree = divident.getDegree();  /// get the degree of the dividend
         }
-        for (Map.Entry<Integer, Monomial> entry : divident.getPolynomial().entrySet()) {
-            int power = entry.getKey();
-            double coefficient = entry.getValue().getCoefficient();
-            remainder.addMonomial(new Monomial(power, coefficient));
+        for (Map.Entry<Integer, Monomial> entry : divident.getPolynomial().entrySet()) {  /// iterate through the dividend
+            int power = entry.getKey();  /// get the power of the current monomial
+            double coefficient = entry.getValue().getCoefficient();  /// get the coefficient of the current monomial
+            remainder.addMonomial(new Monomial(power, coefficient));   /// add the monomial to the remainder
         }
-        return new ArrayList<Polynomial>() {{
+        return new ArrayList<Polynomial>() {{  /// return the quotient and the remainder
             add(quotient);
             add(remainder);
         }};
--- src/main/java/Polynomial/Polynomial.java
+++ src/main/java/Polynomial/Polynomial.java
@@ -19,35 +19,35 @@ public class Polynomial {
     }
 
 
-    public void addMonomial(Monomial monomial) {
-        int power = monomial.getPower();
-        if (polynomial.containsKey(power)) {
-            Monomial existingMonomial = polynomial.get(power);
-            double newCoefficient = existingMonomial.getCoefficient() + monomial.getCoefficient();
-            existingMonomial.setCoefficient(newCoefficient);
+    public void addMonomial(Monomial monomial) {  /// method for adding a monomial to the polynomial
+        int power = monomial.getPower();  /// the power of the monomial
+        if (polynomial.containsKey(power)) {  /// if the polynomial already contains the power of the monomial
+            Monomial existingMonomial = polynomial.get(power);  /// the monomial with the same power
+            double newCoefficient = existingMonomial.getCoefficient() + monomial.getCoefficient();  /// the new coefficient of the monomial
+            existingMonomial.setCoefficient(newCoefficient);  /// the new coefficient is set
         } else {
-            polynomial.put(power, monomial);
+            polynomial.put(power, monomial);  /// if the polynomial does not contain the power of the monomial, the monomial is added to the polynomial
         }
     }
 
 
-    public static Polynomial formPolynomial(String polynomialInput) throws IllegalArgumentException {
-        Polynomial polynomial = new Polynomial();
-        Pattern termPattern = Pattern.compile("([-+]?[0-9]*\\.?[0-9]*)\\*?x\\^([0-9]+)"); // ala bun
-        Matcher matcher = termPattern.matcher(polynomialInput);
-        if (!matcher.find()) {
-            JOptionPane.showMessageDialog(null, "Invalid polynomial format!");
+    public static Polynomial formPolynomial(String polynomialInput) throws IllegalArgumentException {  /// method for creating a polynomial from a string
+        Polynomial polynomial = new Polynomial();  /// a new polynomial is created
+        Pattern termPattern = Pattern.compile("([-+]?[0-9]*\\.?[0-9]*)\\*?x\\^([0-9]+)"); /// the pattern for the monomial
+        Matcher matcher = termPattern.matcher(polynomialInput);  /// the matcher for the monomial
+        if (!matcher.find()) {  /// if the polynomial is not in the correct format
+            JOptionPane.showMessageDialog(null, "Invalid polynomial format!");  /// an error message is displayed
         }
         do {
-            String coefficientString = matcher.group(1);
-            double coefficient = coefficientString.isEmpty() ? 1.0 : Double.parseDouble(coefficientString);
-            String powerString = matcher.group(2);
-            int power = powerString.isEmpty() ? 0 : Integer.parseInt(powerString);
-            Monomial monomial = new Monomial(power, coefficient);
-            polynomial.addMonomial(monomial);
-        } while (matcher.find());
+            String coefficientString = matcher.group(1);  /// the coefficient of the monomial
+            double coefficient = coefficientString.isEmpty() ? 1.0 : Double.parseDouble(coefficientString);  /// if the coefficient is empty, the coefficient is 1 else the coefficient is the coefficient of the monomial
+            String powerString = matcher.group(2);  /// the power of the monomial
+            int power = powerString.isEmpty() ? 0 : Integer.parseInt(powerString);  /// if the power is empty, the power is 0 else the power is the power of the monomial
+            Monomial monomial = new Monomial(power, coefficient);  /// a new monomial is created
+            polynomial.addMonomial(monomial);  /// the monomial is added to the polynomial
+        } while (matcher.find()); /// the monomial is added to the polynomial until there are no more monomials
 
-        return polynomial;
+        return polynomial;  /// the polynomial is returned
     }
 
     public HashMap<Integer, Monomial> getPolynomial() {
@@ -58,25 +58,25 @@ public class Polynomial {
 
 
 
-    public int getDegree() {
-        int degree = 0;
-        for (Map.Entry<Integer, Monomial> entry : polynomial.entrySet()) {
-            int currentDegree = entry.getKey();
-            if (currentDegree > degree) {
-                degree = currentDegree;
+    public int getDegree() {  /// method for getting the degree of the polynomial
+        int degree = 0;  /// the degree of the polynomial
+        for (Map.Entry<Integer, Monomial> entry : polynomial.entrySet()) {  /// for each entry in the polynomial
+            int currentDegree = entry.getKey();  /// the current degree of the monomial
+            if (currentDegree > degree) {  /// if the current degree is greater than the degree of the polynomial
+                degree = currentDegree;  /// the degree of the polynomial is set to the current degree
             }
         }
-        return degree;
+        return degree;  /// the degree of the polynomial is returned
     }
 
-    public double needCoefficient(int power) {
-        if (polynomial.containsKey(power)) {
-            return polynomial.get(power).getCoefficient();
+    public double needCoefficient(int power) {  /// method for getting the coefficient of a monomial with a given power
+        if (polynomial.containsKey(power)) {  /// if the polynomial contains the power of the monomial
+            return polynomial.get(power).getCoefficient();  /// the coefficient of the monomial is returned
         }
         return 0.0;
     }
 
-    public String toString() {
+    public String toString() {  /// method for converting the polynomial to a string
         StringBuilder sb = new StringBuilder();
         for (int power = this.getDegree(); power >= 0; power--) {
             double coefficient = this.needCoefficient(power);
